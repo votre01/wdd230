eb7676aef923c8b0adda19bd7261e37478ba087a
Trevor <ngw21001@byui.edu>
2023-06-07 18:25:01 +0200

    home-page-week01

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+
+const currentYear = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.innerText = new Date().getFullYear();
+lastModified.innerText = `Last Modification: ${document.lastModified}`;

3c3953e0499c7703e10d1c067060584cbf7fe091
Trevor <ngw21001@byui.edu>
2023-07-03 09:39:14 +0200

    week02

--- scripts/script.js
+++ scripts/script.js
@@ -59,7 +59,7 @@ window.addEventListener('load', function() {
 });
 
 
-
+// Range slider for ratings
 const rangevalue = document.getElementById("rangevalue");
 const range = document.getElementById("rating");
 
@@ -69,4 +69,24 @@ range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
+}
+
+const kp1 = document.querySelector("#password");
+const kp2 = document.querySelector("#password2");
+const message = document.querySelector("#message");
+
+kp2.addEventListener("focusout", checkSame);
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "Passwords DO NOT MATCH!";
+		message.style.color = "red";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
 }

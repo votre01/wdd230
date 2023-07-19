32030e1f8ccd16fe5d7e759cb63806f55a0bd85a
Trevor <ngw21001@byui.edu>
2023-07-19 17:19:55 +0200

    week07

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -14,6 +14,9 @@ const directory = document.querySelector('#directory');
 const membersUrl = 'https://votre01.github.io/wdd230/chamber/data/members.json';
 const dirToggle = document.querySelector('#dirToggle');
 
+const spotSelection = document.querySelector('#spotSelection');
+
+
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 hambuttom.addEventListener('click', () => {
     mainnav.classList.toggle('show');
@@ -38,6 +41,35 @@ function checkSame() {
 	}
 }
 
+// Get weather
+const weatherInfo = document.querySelector('#weatherInfo');
+const forecastInfo = document.querySelector('#forecastInfo');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-17.83&lon=31.05&units=imperial&appid=4eb999e92d892ed7069e6c7aec3abf13';
+
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-17.83&lon=31.05&exclude=curent,minutely&appid=4eb999e92d892ed7069e6c7aec3abf13';
+
+async function fetchWeatherApi(url, placement) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data, placement);
+      console.log(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+const displayResults = (weather, placement) => {
+    placement.innerHTML = `<img src="https://openweathermap.org/img/w/${weather.weather[0].icon}.png" alt="${weather.weather[0].main}"> <span>${weather.main.temp}&#8457; - ${weather.weather[0].description}</span>`;
+};
+
+fetchWeatherApi(url, weatherInfo);
+fetchWeatherApi(forecastUrl, forecastInfo);
 
 // Get member data
 async function getMemberData(url) {
@@ -45,10 +77,49 @@ async function getMemberData(url) {
     if (response.ok) {
         const data = await response.json();
         console.log(data);
-        displayMembers(data.members);
+        displayMembers(data.members, directory);
+        // spotlight(data.members);
     }
 }
 
+
+function spotlight(members) {
+    const selection1 = assignSelection(members, selection1);
+    const selection2 = assignSelection(members, selection2);
+
+    const selectedMembers = [members[selection1], members[selection2]];
+    displayMembers(selectedMembers, spotSelection);
+}
+
+const assignSelection = (members, selection) => {
+    let memberIndex = Math.floor(Math.random() * members.length) + 1;
+
+    if (members[memberIndex].membershipLevel == 'Silver' || members[memberIndex].membershipLevel == 'Gold') {
+        selection = memberIndex;
+        console.log(selection);
+    } else {
+        selection = 4;
+    }
+
+    return selection;
+}
+
+const displayMembers = (members, placement) => {    
+    members.forEach((member) => {
+        const directoryCard = document.createElement('div');
+        directoryCard.innerHTML = `<img src="${member.profilePic}" title="${member.name}">
+                                   <h3>${member.name}</h3>
+                                   <p>⭐${member.membershipLevel}⭐</p>
+                                   <p>${member.address}</p>
+                                   <p>${member.phone}</p>
+                                   <a href="${member.website}" target="_blank">Website</a>`;
+        
+        placement.append(directoryCard);
+    });
+};
+
+
+getMemberData(membersUrl);
 
 // Toggle List/Grid view directory
 dirToggle.addEventListener('click', toggleDirectoryView);
@@ -64,21 +135,6 @@ function toggleDirectoryView() {
         dirToggle.textContent = 'List View';
     }
 }
-
-const displayMembers = (members) => {    
-    members.forEach((member) => {
-        const directoryCard = document.createElement('div');
-        directoryCard.innerHTML = `<img src="${member.profilePic}" title="${member.name}">
-                                   <h3>${member.name}</h3>
-                                   <p>⭐${member.membershipLevel}⭐</p>
-                                   <p>${member.address}</p>
-                                   <p>${member.phone}</p>
-                                   <a href="${member.website}" target="_blank">Website</a>`;
-        directory.append(directoryCard);
-    });
-};
-
-getMemberData(membersUrl);
 
 // Local Storage
 const now = new Date();
